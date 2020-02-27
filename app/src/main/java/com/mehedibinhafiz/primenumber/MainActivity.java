a1ee3473b9a7d4a94082c5e7dbc88b27b16a9d97
mehedibinhafiz <mehedibinhafiz@gmail.com>
2020-02-27 14:42:03 +0600

    about and Intent

--- app/src/main/java/com/mehedibinhafiz/primenumber/MainActivity.java
+++ app/src/main/java/com/mehedibinhafiz/primenumber/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mehedibinhafiz.primenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -48,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Enater a valid number",Toast.LENGTH_LONG).show();
         }
 
+    }
+    public void about(View view){
+        Intent intent = new Intent(MainActivity.this,About.class);
+        startActivity(intent);
     }
 
 
